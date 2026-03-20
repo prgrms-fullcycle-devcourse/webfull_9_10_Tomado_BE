1f49c9dac4e8de53e0c07dc2c0b8be93bca4de59
taeseongxxxx <taeseongxxxx@gmail.com>
2026-03-20 15:09:21 +0900

    fix: env.ts 수정

--- src/env.ts
+++ src/env.ts
@@ -2,19 +2,12 @@ import * as dotenv from 'dotenv';
 import fs from 'node:fs';
 import { z } from 'zod';
 
-const localEnvPath = 'env/local.env';
-const exampleEnvPath = 'env/example.env';
-
-if (fs.existsSync(localEnvPath)) {
-  dotenv.config({ path: localEnvPath });
-} else {
-  dotenv.config({ path: exampleEnvPath });
-}
+dotenv.config();
 
 const EnvSchema = z.object({
-  NODE_ENV: z.string().default('development'),
-  PORT: z.coerce.number().default(4000),
-  DATABASE_URL: z.string().min(1),
+    NODE_ENV: z.string().default('development'),
+    PORT: z.coerce.number().default(4000),
+    DATABASE_URL: z.string().min(1),
 });
 
 export const env = EnvSchema.parse(process.env);
